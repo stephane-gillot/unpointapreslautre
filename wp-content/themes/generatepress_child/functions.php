dbec2ff32f2a1cb88e00545e3d2891c290940b3b
sgillot <gillot1969@gmail.com>
2022-07-02 08:26:16 +0200

    add footer filter and sass capabilities

--- wp-content/themes/generatepress_child/functions.php
+++ wp-content/themes/generatepress_child/functions.php
@@ -10,5 +10,25 @@ function generatepress_child_enqueue_scripts() {
 	if ( is_rtl() ) {
 		wp_enqueue_style( 'generatepress-rtl', trailingslashit( get_template_directory_uri() ) . 'rtl.css' );
 	}
+
 }
 add_action( 'wp_enqueue_scripts', 'generatepress_child_enqueue_scripts', 100 );
+
+function child_theme_enqueue_style() {
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'custom-style', get_stylesheet_directory_uri() . '/assets/css/main.css?v=' . time(), array( 'parent-style' ), time() );
+
+}
+add_action( 'wp_enqueue_scripts', 'child_theme_enqueue_style', 100 );
+
+//++++++++++++++++++++
+// Filters
+//++++++++++++++++++++
+add_filter( 'generate_copyright', 'upala_custom_copyright' );
+function upala_custom_copyright() {
+	return sprintf(
+		'<span class="copyright">&copy; %1$s %2$s</span>',
+		date( 'Y' ),
+		get_bloginfo( 'name' )
+	);
+}
